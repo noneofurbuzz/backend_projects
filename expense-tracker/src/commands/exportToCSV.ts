0618cdfcf7d6022ce54fd67e7fb9b66d4b689c52
Ugochi Ike <ugochiike26@gmail.com>
2024-11-25 22:53:50 +0100

    feat: allow exports to csv file

---
+++ expense-tracker/src/commands/exportToCSV.ts
@@ -0,0 +1,38 @@
+import fs from 'fs'
+import { readFile } from "../utils/expensesUtils.js"
+import chalk from 'chalk'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import open from 'open'
+
+
+function writeCSV(file_path: string,data:string){
+    fs.writeFile(file_path,data,(err) => {
+        if (err) throw err
+    })
+}
+
+export default (file_path: string) => {
+
+let data = readFile()
+if (data.length === 0){
+    console.error('There are no expenses yet. Use add [options] to add a new expense.')
+    console.error(("Sample: expense-tracker add --description 'Lunch' --amount 20"))
+    return
+}
+let columns = `id,description,amount,date,category`
+for (let i = 0;i < data.length; i = i + 1){
+    data[i].currency = ""
+    columns = columns + "\n" + Object.values(data[i])
+}
+
+if (process.argv.slice(3)[0].includes('.csv') === false){
+    console.error(chalk.red('Error: only .csv files can be processed at this time'))
+    console.error(`Sample: expense-tracker export expenses.csv`)
+    return
+}
+else{
+    writeCSV(file_path,columns)
+    open(`${path.dirname(path.dirname(path.dirname(fileURLToPath(import.meta.url))))+'/'+file_path}`)
+}
+}
