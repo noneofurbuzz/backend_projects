344f29181ecd043307b20f379da785e20d5c57f1
Ugochi Ike <ugochiike26@gmail.com>
2024-12-09 12:29:36 +0100

    feat: get messages from backend

--- mini-message-board/client/src/App.tsx
+++ mini-message-board/client/src/App.tsx
@@ -1,9 +1,40 @@
-
+import axios, { AxiosError, AxiosResponse } from "axios"
+import { useEffect, useState } from "react"
+import { Loader } from "./components/Loader"
+import {messages} from '@backend/types.ts'
 
 function App() {
+  const [messages,setMessages] = useState<messages[]>([])
+  const [loading,setLoading] = useState(true)
+  useEffect(() => {
+    axios.get('http://localhost:3000/').then((response: AxiosResponse) => {
+    const data: messages[] = response.data
+    setMessages(data)
+  }).catch((error: Error | AxiosError) => {
+    console.log(error.message)
+  })
+  const timeOut = setTimeout(() => {
+    setLoading(false)
+  },2000)
+  return (() => {
+    clearTimeout(timeOut)
+  })
+  },[])
+  
   return (
     <>
-      
+    {loading ? <Loader /> :
+    messages.length !=0 ? messages.map((message) => {
+      return( 
+      <div key={message.id}>
+      <h1>{message?.user}</h1>
+      <h1>{message?.text}</h1>
+      <h1>{message.added.toString()}</h1>
+      </div>
+      )
+      }) :
+      <h1>No messages here</h1>
+      }
     </>
   )
 }
---
+++ mini-message-board/client/src/components/Loader.tsx
@@ -0,0 +1,5 @@
+export function Loader(){
+    return(
+        <h1>Loading...</h1>
+    )
+}
